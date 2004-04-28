1f43c04930d54dbbf8c1648d02af990ca3a239a2
Gavin Brown <gbrown@cpan.org>
2004-04-28 19:52:56 +0000

    Import of GBROWN/Bundle-Gnome2-0.06 from CPAN.
    gitpan-cpan-distribution: Bundle-Gnome2
    gitpan-cpan-version:      0.06
    gitpan-cpan-path:         GBROWN/Bundle-Gnome2-0.06.tar.gz
    gitpan-cpan-author:       GBROWN
    gitpan-cpan-maturity:     released

--- Gnome2.pm
+++ Gnome2.pm
@@ -1,7 +1,7 @@
-# $Id: Gnome2.pm,v 1.8 2003/12/14 14:02:08 jodrell Exp $
+# $Id: Gnome2.pm,v 1.9 2004/04/28 09:40:09 jodrell Exp $
 package Bundle::Gnome2;
 
-$VERSION = '0.05';
+$VERSION = '0.06';
 
 __END__
 
@@ -33,15 +33,15 @@ Gtk2::PodViewer			- a Gtk2 widget for displaying Plain old Documentation (POD)
 
 Gtk2::TrayIcon			- Perl interface to the EggTrayIcon library
 
-Gnome2				- Perl interface to the 2.x series of the GNOME libraries
-
 Gnome2::Canvas			- Perl interface to the Gnome Canvas
+
+Gnome2::VFS			- Perl interface to the 2.x series of the GNOME VFS library
+
+Gnome2				- Perl interface to the 2.x series of the GNOME libraries
 
 Gnome2::GConf			- Perl wrappers for the GConf configuration engine
 
 Gnome2::Print			- Perl wrappers for the Gnome Print utilities
-
-Gnome2::VFS			- Perl interface to the 2.x series of the GNOME VFS library
 
 Gnome2::Vte			- Perl interface to the VTE library
 
@@ -60,7 +60,7 @@ libwnck.
 As of version 0.05, the Gtk2::Spell module has been removed. This is
 because Gtk2::Spell does not reliably build on all platforms (due to
 incompatibilities between pspell and aspell, and varying support for
-each among distributors. This does not make this module well-suited to
+each among distributors). This does not make this module well-suited to
 an automated install system.
 
 See
